199149bfa6f941e05460acdcac660369306a3e94
istomin10593 <istomin.artem.10593@gmail.com>
2022-11-22 19:37:22 +0000

    Started to implement CreateUser

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,24 @@
 package users
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/istomin10593/bookstore_users-api/domain/users"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	var user users.User
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		return
+	}
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		return
+	}
 	c.String(http.StatusNotImplemented, "implement me!")
 }
 
